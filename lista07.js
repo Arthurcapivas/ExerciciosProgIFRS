a08075927aea46225953222d8828783d8fa5678e
Arthurcapivas <10160273@aluno.restinga.ifrs.edu.br>
2024-04-25 17:53:38 -0300

    correção

--- lista07.js
+++ lista07.js
@@ -5,7 +5,7 @@ var raio = 0
 
 raio = parseFloat(entradaNode.question("qual o raio do circulo? "))
 
-area = Math.PI * raio * raio 
+area = Math.PI * raio * raio //Math.PI representa a proporção entre circunferência de um círculo com o seu diâmetro, proximadamente 3.14159
 area = area * 2
 
 console.log(`a area do circulo é: ${area.toFixed(0)}`)
--- lista10.js
+++ lista10.js
@@ -1,7 +1,19 @@
 var entradaNode = require('readline-sync');
 
+
+//variaveis dos numeros iniciais
 var int1 = 0
 var int2 = 0
-var real = 0
+var real = 0.0
 
+//entrad de dados (pergunta de quais os números)
+int1 = parseInt(entradaNode.question("Digite um numero inteiro: "))
+int2 = parseInt(entradaNode.question("Digite um numero inteiro: "))
+real = parseFloat(entradaNode.question("Digite um numero real: "))
 
+//variaveis para o produto, soma e cubo das equações 
+var produto = (2 * int1) * (int2 / 2)
+var soma = (3 * int1) + real
+var cubo = Math.pow(real, 3) //Math.pow(base, expoente)
+
+console.log(`O produto do dobro do primeiro com metade do segundo é ${produto} \nA soma do triplo do primeiro com o terceiro é ${soma} \nO terceiro elevado ao cubo é ${cubo}`) 
